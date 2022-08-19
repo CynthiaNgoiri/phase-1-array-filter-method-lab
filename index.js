48c909e3346a42ba2b8dd695d97bcc379393f3b8
Cynthia Njomo <cynthiangoirinjomo@gmail.com>
2022-08-19 23:21:23 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(name, string) {return name.filter((name) => {
+        return name.toLowerCase() === string.toLowerCase();
+    });
+}
+
+function fuzzyMatch(name, string) {return name.filter((name) =>{
+        return name.slice(0, string.length) === string;
+    })
+}
+
+function matchName(object, string) {return object.filter((object) => {
+    return object.name === string;
+    })
+}
